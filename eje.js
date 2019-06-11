875a78e1b8c055aa7799ed218ff79d1a23a643d7
IvanJason134 <jestrada1@ucol.mx>
2019-06-11 05:26:45 -0500

    Nuevo atributo agregado

--- eje.js
+++ eje.js
@@ -1,7 +1,8 @@
 class eje {
-    constructor(cosa1, cosa2, cosa3) {
+    constructor(cosa1, cosa2, cosa3, cosa4) {
         this._cosa1 = cosa1;
         this._cosa2 = cosa2;
         this._cosa3 = cosa3;
+        this._cosa4 = cosa4;
     }
 }
